d6803113d924f738cdf9b0433961f2dc065c1418
adventurous-format-6947 <bminczu@gmail.com>
2020-10-07 17:11:03 +0000

    Done.

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,15 +1,15 @@
 def join_nested_strings(src)
   row_index=0
-  string_array = ""
+  string_hash = []
   while row_index<src.count do
     element_index=0
     while element_index<src[row_index].count do
-      if src[row_index][element].class==string
-        string_array << src[row_index][element_index]
+      if src[row_index][element_index].class==String
+        string_hash << src[row_index][element_index]
     end
     element_index+=1
   end
   row_index +=1
 end
-string_array
+string_hash.join(' ')
 end
